4723c24b66f2673f79f77769d93db6628d02c96b
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-01-29 19:35:34 +0700

    (fix) add new /customer web route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
@@ -23,3 +24,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/', [DashboardController::
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/customer', [CustomerController::class, 'index'])->name('customer.index');
