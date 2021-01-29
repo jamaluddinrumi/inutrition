2d10d7eee16412f0e88115d1222c1284615d3003
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-01-29 19:34:14 +0700

    (fix) create new page Customer.vue

---
+++ resources/js/Pages/Customer.vue
@@ -0,0 +1,67 @@
+<template>
+    <layout>
+        <v-row>
+            <v-col>
+                <customers-table :items-per-page="25"></customers-table>
+            </v-col>
+        </v-row>
+    </layout>
+</template>
+<script>
+import Layout from "@/Layouts/Layout";
+import CustomersTable from "@/Components/Table/Customers"
+
+export default {
+    title() {
+        if (this.title) {
+            let translationTitle = this.$vuetify.lang.t(
+                "$vuetify.title." + this.title
+            );
+            if (translationTitle) {
+                return translationTitle;
+            } else {
+                return _.capitalize(this.title);
+            }
+        }
+    },
+
+    components: {
+        Layout,
+        CustomersTable,
+    },
+
+    props: {
+        currentRouteName: {
+            type: String,
+        },
+        errorBags: {
+            type: Array,
+        },
+        errors: {
+            type: Object,
+        },
+        jetstream: {
+            type: Object,
+        },
+        user: {
+            type: Object,
+        },
+        customers: {
+            type: Array,
+        },
+        title: {
+            type: String,
+        },
+    },
+
+    data() {
+        return {};
+    },
+
+    computed: {},
+
+    watch: {},
+
+    methods: {},
+};
+</script>
