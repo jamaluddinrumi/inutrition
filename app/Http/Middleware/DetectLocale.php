0de3f581f277b41643aa9a9c018a3b8bb604589e
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-01-31 14:06:37 +0700

    (fix) create DetectLocale middleware

---
+++ app/Http/Middleware/DetectLocale.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+
+class DetectLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if (session('locale')) {
+            App::setLocale(session('locale'));
+        } else {
+            App::setLocale(config('app.fallback_locale'));
+        }
+
+        return $next($request);
+    }
+}
