67dd6e0272a9ee990f2b7ef3d1cfdecbea2f3663
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-02-02 12:14:32 +0700

    Add graphic placeholder for future development

---
+++ resources/js/Components/Card/Graphic.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card class="mx-auto text-center" :color="color" dark max-width="600">
+        <v-card-text>
+            <v-sheet color="rgba(0, 0, 0, .12)">
+                <v-sparkline
+                    :value="data"
+                    color="rgba(255, 255, 255, .7)"
+                    height="100"
+                    padding="24"
+                    stroke-linecap="round"
+                    smooth
+                >
+                    <template v-slot:label="item"> {{ item.value }} </template>
+                </v-sparkline>
+            </v-sheet>
+        </v-card-text>
+
+        <v-card-text>
+            <div class="display-1 font-weight-thin">{{ title }}</div>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="justify-center">
+            <v-btn block text>{{ $vuetify.lang.t("$vuetify.readMore") }}</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        data: {
+            type: Array,
+        },
+        color: {
+            type: String,
+            default: "primary",
+        },
+        title: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
+
+<style>
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,11 +1,22 @@
 <template>
     <layout>
+        <v-row>
+            <v-col v-for="graphicData in graphicsData" :key="graphicData.id">
+                <graphic-card
+                    :data="graphicData.data"
+                    :color="graphicData.color"
+                    :title="graphicData.title"
+                ></graphic-card>
+            </v-col>
+        </v-row>
         <div class="text-h4 mt-6 mb-4">
             {{ $vuetify.lang.t("$vuetify.customersNutrition") }}
         </div>
         <v-row>
             <v-col>
-                <dashboard-customers-nutritions-table :items-per-page="15"></dashboard-customers-nutritions-table>
+                <dashboard-customers-nutritions-table
+                    :items-per-page="15"
+                ></dashboard-customers-nutritions-table>
             </v-col>
         </v-row>
         <div class="text-h4 mt-6 mb-4">
@@ -22,6 +33,7 @@
 import Layout from "@/Layouts/Layout";
 import DashboardCustomersNutritionsTable from "@/Components/Table/DashboardCustomersNutritions";
 import ContactCard from "@/Components/Card/Contact";
+import GraphicCard from "@/Components/Card/Graphic";
 
 export default {
     title() {
@@ -41,6 +53,7 @@ export default {
         Layout,
         DashboardCustomersNutritionsTable,
         ContactCard,
+        GraphicCard,
     },
 
     props: {
@@ -71,7 +84,22 @@ export default {
     },
 
     data() {
-        return {};
+        return {
+            graphicsData: [
+                {
+                    id: 0,
+                    data: [423, 446, 675, 510, 590, 610, 760],
+                    color: "green",
+                    title: this.$vuetify.lang.t("$vuetify.positiveTrend"),
+                },
+                {
+                    id: 1,
+                    data: [760, 610, 590, 510, 675, 446, 423],
+                    color: "red",
+                    title: this.$vuetify.lang.t("$vuetify.negativeTrend"),
+                },
+            ],
+        };
     },
 
     computed: {},
