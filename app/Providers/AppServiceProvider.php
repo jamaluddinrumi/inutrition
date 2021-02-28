b9470d24545f6d95344440630abe15554909c297
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-02-28 14:57:54 +0700

    Add locale props

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
@@ -30,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
         Inertia::share('storagePath', storage_path());
         Inertia::share('publicPath', public_path());
         Inertia::share('storageUrl', asset('storage'));
+        Inertia::share('locale', App::getLocale());
 
         JsonResource::withoutWrapping();
     }
