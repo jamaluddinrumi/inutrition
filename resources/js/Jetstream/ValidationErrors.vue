3d36d42d62b366c98d3119f7b3cbf0f7ed83b5df
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-02-28 13:57:12 +0700

    Fix whoops translation in Validation Errors component

--- resources/js/Jetstream/ValidationErrors.vue
+++ resources/js/Jetstream/ValidationErrors.vue
@@ -1,34 +1,41 @@
 <template>
     <div v-if="hasErrors">
-        <div class="font-medium text-red-600">Whoops! Something went wrong.</div>
+        <div class="font-medium text-red-600">
+            {{ $vuetify.lang.t("$vuetify.whoops") }}
+        </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
-            <li v-for="(error, key) in flattenedErrors" :key="key">{{ error }}</li>
+            <li v-for="(error, key) in flattenedErrors" :key="key">
+                {{ error }}
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            bag: {
-                type: String,
-                default: 'default',
-            },
+export default {
+    props: {
+        bag: {
+            type: String,
+            default: "default",
+        },
+    },
+
+    computed: {
+        errorBag() {
+            return this.$page.props.errorBags[this.bag] || {};
         },
 
-        computed: {
-            errorBag() {
-                return this.$page.props.errorBags[this.bag] || {}
-            },
+        hasErrors() {
+            return Object.keys(this.errorBag).length > 0;
+        },
 
-            hasErrors() {
-                return Object.keys(this.errorBag).length > 0;
-            },
-
-            flattenedErrors() {
-                return Object.keys(this.errorBag).reduce((carry, key) => carry.concat(this.errorBag[key]), [])
-            }
-        }
-    }
+        flattenedErrors() {
+            return Object.keys(this.errorBag).reduce(
+                (carry, key) => carry.concat(this.errorBag[key]),
+                []
+            );
+        },
+    },
+};
 </script>
