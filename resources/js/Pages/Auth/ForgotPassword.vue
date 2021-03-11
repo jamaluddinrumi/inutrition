7a8030415d9b8334b9f1d7db9e5c56f69d4e9bce
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-03-11 21:24:45 +0700

    Change login instead text in forgot password page

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -33,13 +33,9 @@
                         <div class="flex items-center justify-end mt-4">
                             <inertia-link
                                 :href="route('login')"
-                                class="text-button text-decoration-underline"
+                                class="text-button text-decoration-underline text-opacity-60 dark:text-white dark:text-opacity-70"
                             >
-                                {{
-                                    $vuetify.lang.t(
-                                        "$vuetify.alreadyRegistered"
-                                    )
-                                }}
+                                {{ $vuetify.lang.t("$vuetify.login") }}?
                             </inertia-link>
                             <v-btn
                                 rounded
