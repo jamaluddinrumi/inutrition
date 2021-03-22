cc328dca101dd4b6905992eaa94fa2dfe1aedd85
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-03-22 13:31:34 +0700

    Replace SF font with Roboto font in auth card logo

--- resources/js/Jetstream/AuthenticationCardLogo.vue
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -1,15 +1,15 @@
 <template>
     <inertia-link href="/" class="no-underline">
-        <svg width="187px" height="32px" viewBox="0 0 187 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <svg width="172px" height="32px" viewBox="0 0 172 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <!-- Generator: Sketch 63.1 (92452) - https://sketch.com -->
             <title>inutrition</title>
             <desc>Created with Sketch.</desc>
             <g id="inutrition" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-size="32.04">
+                <text id="iNutrition" font-family="Roboto-Bold, Roboto" font-weight="bold" fill="#333333">
+                    <tspan x="36" y="29">iNutrition</tspan>
+                </text>
                 <text id="🍇" font-family="AppleColorEmoji, Apple Color Emoji" font-weight="normal" fill="#000000">
                     <tspan x="0" y="30">🍇</tspan>
-                </text>
-                <text id="iNutrition" font-family="SFProText-Bold, SF Pro Text" font-weight="bold" fill="#000000">
-                    <tspan x="36" y="29">iNutrition</tspan>
                 </text>
             </g>
         </svg>
