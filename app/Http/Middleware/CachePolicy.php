464b5de72a886b188d1c09825b4f2852aa61ecea
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-04-02 03:29:58 +0700

    Add CachePolicy middleware

---
+++ app/Http/Middleware/CachePolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CachePolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // return $next($request);
+        $response = $next($request);
+        return $response->header('Cache-Control', 'max-age=300, public');
+    }
+}
