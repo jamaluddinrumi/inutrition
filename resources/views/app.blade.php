9b1bcbe9cc9417822d63448a8079f32c2a5bcc62
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-04-02 21:35:55 +0700

    Add meta description to auth layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <meta name="description" content="{{ env('APP_DESCRIPTION', 'Nutrition Management App') }}">
+
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
